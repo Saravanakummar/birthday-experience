d5ed185992c32ccab0d91b1c33239005219af55b
Saravana Kumaar <saravanakumaar023@gmail.com>
2026-06-30 08:22:24 +0530

    Create Home component for Birthday Experience

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MainScene from "./components/scenes/Scene1"; // Or whichever scene is your first one
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to the Birthday Experience</h1>
+      {/* You can render your initial scene component here */}
+    </main>
+  );
+}
